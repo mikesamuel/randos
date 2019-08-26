daabfd278f3b810c19d493344c0f4b9fd65aff40
Mike Samuel <mikesamuel@gmail.com>
2019-08-26 17:43:15 -0400

    support for dynamic content generation

---
+++ html.js
@@ -0,0 +1,30 @@
+class HTML {
+  constructor(content) {
+    this.content = content;
+  }
+  toString() {
+    return this.content;
+  }
+}
+
+function escHtml(x) {
+  if (x instanceof HTML) { return x; }
+  return new HTML(String(x).replace(/[&<>"']/g, (x) => `&#${ x.charCodeAt(0) };`));
+}
+
+function html(statics, ...dynamics) {
+  return new HTML(combine(statics, dynamics, escHtml));
+}
+html.join = function(arr, delim = '') {
+  return new HTML(arr.map(escHtml).join(escHtml(delim)));
+};
+
+function combine({ raw }, dynamics, esc) {
+  let str = '';
+  const n = dynamics.length;
+  for (let i = 0; i < n; ++i) {
+    str += raw[i] + esc(dynamics[i]);
+  }
+  str += raw[n];
+  return str;
+}
